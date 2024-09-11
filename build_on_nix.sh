9ce9b9bb56231af629ef51ac090319722bc20f35
Azathothas <AjamX101@gmail.com>
2024-09-11 22:22:53 +0545

    pre upx

--- build_on_nix.sh
+++ build_on_nix.sh
@@ -155,17 +155,15 @@ sudo chroot "${ALPINE_CHROOT}" bash -c '
   rm rust-toolchain* 2>/dev/null
   cargo build --target "$RUST_TARGET" --release --jobs="$(($(nproc)+1))" --keep-going
   find "./target/$RUST_TARGET/release" -maxdepth 1 -type f -exec file -i "{}" \; | grep "application/.*executable" | cut -d":" -f1 | xargs realpath | xargs -I {} cp --force {} /build-bins/
+  find "/build-bins/" -type f -exec strip --strip-debug --strip-dwo --strip-unneeded -R ".comment" -R ".gnu.version" --preserve-dates "{}" \; 2>/dev/null
+  find "/build-bins/" -type f -exec bash -c '\''mv "$0" "${0}-$(uname -m)-$(uname -s)"'\'' {} \; 2>/dev/null
+  find "/build-bins/" -type f | xargs realpath | xargs -I {} upx --best "{}" -f --force-overwrite -o"{}.upx" -qq 2>/dev/null
   popd >/dev/null 2>&1
 '
 sudo rsync -av --copy-links --exclude="*/" "${ALPINE_CHROOT}/build-bins/." "${ARTIFACTS}"
 PKG_VERSION="$(cat "${ARTIFACTS}/VERSION.txt")" && export PKG_VERSION="${PKG_VERSION}"
 sudo chown -R "$(whoami):$(whoami)" "${ARTIFACTS}" && chmod -R 755 "${ARTIFACTS}"
 find "${ARTIFACTS}" -type f -name '*.sh' -delete 2>/dev/null
-find "${ARTIFACTS}" -type f ! -name '*.txt' -exec bash -c 'mv "$0" "${0}-$(uname -m)-$(uname -s)"' {} \; 2>/dev/null
-#Strip
-find "${ARTIFACTS}" -type f -exec strip --strip-debug --strip-dwo --strip-unneeded -R ".comment" -R ".gnu.version" --preserve-dates "{}" \; 2>/dev/null
-#upx
-find "${ARTIFACTS}" -type f | xargs realpath | xargs -I {} upx --best "{}" -f --force-overwrite -o"{}.upx" -qq 2>/dev/null
 #End
 popd >/dev/null 2>&1
 ##-------------------------------------------------------#
